461c5c091607030bc5e8ddbe77c77d11b1002bf7
vs.almeida <vs.almeida@icloud.com>
2018-04-26 18:03:18 -0400

    teste do cel

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "JAPONES NAO MANJA NADA DE DEVOPS.")
+        self.assertRegex(result.data.decode(), "O BAULE NAO MANJA NADA DE DEVOPS.")
 
 
 if __name__ == "__main__":
